d17cc74c0d5164ef20928df1f9fe532391f693f2
wgz <490480770@qq.com>
2016-04-22 16:44:59 +0800

    4/22

---
+++ app/src/main/java/wgz/ant/anttongxunlu/GroupManagerActivity.java
@@ -0,0 +1,73 @@
+package wgz.ant.anttongxunlu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import wgz.ant.anttongxunlu.adapter.RecycleAdapter;
+import wgz.ant.anttongxunlu.db.DatabaseHelper;
+
+public class GroupManagerActivity extends AppCompatActivity {
+
+    DatabaseHelper dbh ;
+    List<Map<String, Object>> data1 = new ArrayList<Map<String, Object>>();
+    private TextView maddgroup;
+    private RecyclerView grouplist;
+    RecycleAdapter adapter;
+    CoordinatorLayout gmrootview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_manager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("分组管理");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        init();
+    }
+
+    private void init() {
+        gmrootview = (CoordinatorLayout) findViewById(R.id.gmana_rootview);
+        //maddgroup = (TextView) findViewById(R.id.addgroup);
+        grouplist = (RecyclerView) findViewById(R.id.recycler_view);
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab3);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //数据是使用Intent返回
+                Intent intent = new Intent();
+                //把返回数据存入Intent
+                intent.putExtra("result", "该刷新了");
+                //设置返回数据
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });*/
+        flush();
+
+    }
+
+    private void flush() {
+
+    }
+
+    @Override
+    public void finish() {
+        //数据是使用Intent返回
+        Intent intent = new Intent();
+        //把返回数据存入Intent
+        intent.putExtra("result", "该刷新了");
+        //设置返回数据
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
+}
---
+++ app/src/main/java/wgz/ant/anttongxunlu/adapter/RecycleAdapter.java
@@ -0,0 +1,88 @@
+package wgz.ant.anttongxunlu.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+import wgz.ant.anttongxunlu.R;
+
+/**
+ * Created by qwerr on 2015/11/25.
+ */
+public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewholder> {
+    private Context context;
+    private List<Map<String , Object>> data;
+    private OnItemClickLitener mOnItemClickLitener;
+
+    public RecycleAdapter(List<Map<String, Object>> data,Context context) {
+        this.context = context;
+        this.data = data;
+    }
+
+    public interface OnItemClickLitener
+    {
+        void onItemClick(View view, int position);
+
+    }
+    public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener)
+    {
+        this.mOnItemClickLitener = mOnItemClickLitener;
+    }
+    @Override
+    public MyViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+        MyViewholder holder  = new MyViewholder(LayoutInflater.from(
+                context).inflate(R.layout.recycler_item, parent,
+                false));
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewholder holder, int position) {
+        holder.textView1.setText(data.get(position).get("gname").toString());
+        holder.pid.setText(data.get(position).get("pid").toString());
+        holder.id.setText(data.get(position).get("id").toString());
+        if (mOnItemClickLitener!=null){
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickLitener.onItemClick(holder.itemView, pos);
+                }
+            });
+
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public  class MyViewholder extends RecyclerView.ViewHolder {
+        TextView textView1;
+        TextView pid;
+        TextView id;
+
+        public MyViewholder(View itemView) {
+            super(itemView);
+            textView1 = (TextView) itemView.findViewById(R.id.mangroup_name);
+            pid = (TextView) itemView.findViewById(R.id.gmpid);
+            id = (TextView) itemView.findViewById(R.id.gmid);
+
+
+        }
+
+
+
+    }
+
+}
